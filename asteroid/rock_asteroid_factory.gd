1bcfb7bb57ebc8bda6ef2864abbab26d398da47a
apBUSampK <sdsnm737@gmail.com>
2023-06-22 17:23:35 +0300

    added bg textures

--- asteroid/rock_asteroid_factory.gd
+++ asteroid/rock_asteroid_factory.gd
@@ -1,6 +1,6 @@
 extends RefCounted
 
-var atlas = preload("res://asteroid/textures/atlas1.png")
+var atlas = preload("res://textures/atlas1.png")
 var material_rock = preload("res://asteroid/rock.tres")
 var a_size = atlas.get_size()[0]
 
@@ -19,7 +19,7 @@ func simple_gen(v: int, r0: float, dr: float, dphi: float) -> RigidBody2D:
 	var cpolygon = CollisionPolygon2D.new()
 	var polygon = Polygon2D.new()
 	cpolygon.set_polygon(poly_array)
-	polygon.set_polygon(poly_array)
+	polygon.set_polygon(_bezier_t(poly_array, .8, .16).tessellate(2))
 	polygon.set_texture(atlas)
 	polygon.set_texture_offset(Vector2(randf_range(0, a_size), randf_range(0, a_size)))
 	polygon.set_texture_repeat(CanvasItem.TEXTURE_REPEAT_ENABLED)
@@ -28,7 +28,8 @@ func simple_gen(v: int, r0: float, dr: float, dphi: float) -> RigidBody2D:
 	asteroid.add_child(cpolygon)
 	asteroid.add_child(polygon)
 	return asteroid
-	
+
+
 func bulge_gen(v: int, r0: float, dr: float, dphi: float, bulk_r: float, size: int, incl_size: float) -> RigidBody2D:
 	var phi_step = 2 * PI / v
 	dphi *= phi_step
@@ -53,7 +54,7 @@ func bulge_gen(v: int, r0: float, dr: float, dphi: float, bulk_r: float, size: i
 	var cpolygon = CollisionPolygon2D.new()
 	var polygon = Polygon2D.new()
 	cpolygon.set_polygon(poly_array)
-	polygon.set_polygon(poly_array)
+	polygon.set_polygon(_bezier_t(poly_array, .8, .16).tessellate(2))
 	polygon.set_texture(atlas)
 	polygon.set_texture_offset(Vector2(randf_range(0, a_size), randf_range(0, a_size)))
 	polygon.set_texture_repeat(CanvasItem.TEXTURE_REPEAT_ENABLED)
@@ -62,3 +63,17 @@ func bulge_gen(v: int, r0: float, dr: float, dphi: float, bulk_r: float, size: i
 	asteroid.add_child(cpolygon)
 	asteroid.add_child(polygon)
 	return asteroid
+
+
+func _bezier_t(arr: PackedVector2Array, cl: float, c_d: float) -> Curve2D:
+	var _cl = 1 - cl
+	var curve = Curve2D.new()
+	for i in range(len(arr) - 1):
+		var curr = arr[i]
+		var prev = arr[i - 1]
+		var next = arr[i + 1]
+		curve.add_point(cl * curr + _cl * prev, c_d * (prev - curr), c_d * (curr - prev))
+		curve.add_point(cl * curr + _cl * next, c_d * (curr - next), c_d * (next - curr))
+	curve.add_point(cl * arr[-1] + _cl * arr[-2], c_d * (arr[-2] - arr[-1]), c_d * (arr[-1] - arr[-2])) #last point
+	curve.add_point(cl * arr[-1] + _cl * arr[0], c_d * (arr[-1] - arr[0]), c_d * (arr[0] - arr[-1]))
+	return curve
