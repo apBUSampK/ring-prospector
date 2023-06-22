153a7451d5b8ab08986e1956c11b6e4ace6fd294
apBUSampK <sdsnm737@gmail.com>
2023-06-22 15:38:18 +0300

    factory + texturing

---
+++ asteroid/rock_asteroid_factory.gd
@@ -0,0 +1,64 @@
+extends RefCounted
+
+var atlas = preload("res://asteroid/textures/atlas1.png")
+var material_rock = preload("res://asteroid/rock.tres")
+var a_size = atlas.get_size()[0]
+
+func simple_gen(v: int, r0: float, dr: float, dphi: float) -> RigidBody2D:
+	var phi_step = 2 * PI / v
+	dphi *= phi_step
+	var poly_array = PackedVector2Array()
+	for i in range(v):
+		var r_delta = randfn(0, dr)
+		var phi_delta = phi_step + randf_range(-dphi, dphi)
+		r_delta = r_delta if abs(r_delta) < phi_delta * r0 / 3 else phi_delta * r0 / 3 * sign(r_delta)
+		var phi = phi_delta + phi_step * (i - 1)
+		var r = r0 + r_delta
+		poly_array.append(Vector2(r * cos(phi), r * sin(phi)))
+	var asteroid = RigidBody2D.new()
+	var cpolygon = CollisionPolygon2D.new()
+	var polygon = Polygon2D.new()
+	cpolygon.set_polygon(poly_array)
+	polygon.set_polygon(poly_array)
+	polygon.set_texture(atlas)
+	polygon.set_texture_offset(Vector2(randf_range(0, a_size), randf_range(0, a_size)))
+	polygon.set_texture_repeat(CanvasItem.TEXTURE_REPEAT_ENABLED)
+	asteroid.set_mass(r0**3)
+	asteroid.set_physics_material_override(material_rock)
+	asteroid.add_child(cpolygon)
+	asteroid.add_child(polygon)
+	return asteroid
+	
+func bulge_gen(v: int, r0: float, dr: float, dphi: float, bulk_r: float, size: int, incl_size: float) -> RigidBody2D:
+	var phi_step = 2 * PI / v
+	dphi *= phi_step
+	incl_size = incl_size * size / 2
+	var bulk_dr = bulk_r / incl_size
+	var poly_array = PackedVector2Array()
+	var offset = Vector2(cos(size * phi_step / 2), sin(size * phi_step / 2)) * bulk_r * size / v / 2
+	for i in range(v):
+		var r_delta = randfn(0, dr)
+		var phi_delta = phi_step + randf_range(-dphi, dphi)
+		r_delta = r_delta if abs(r_delta) < phi_delta * r0 / 3 else phi_delta * r0 / 3 * sign(r_delta)
+		var phi = phi_delta + phi_step * (i - 1)
+		var r = r0 + r_delta
+		if i < size:
+			r += bulk_r
+			if i <= incl_size:
+				r -= (incl_size - i) * bulk_dr
+			if i >= size - incl_size:
+				r += (size - i - incl_size) * bulk_dr
+		poly_array.append(Vector2(r * cos(phi), r * sin(phi)) - offset)
+	var asteroid = RigidBody2D.new()
+	var cpolygon = CollisionPolygon2D.new()
+	var polygon = Polygon2D.new()
+	cpolygon.set_polygon(poly_array)
+	polygon.set_polygon(poly_array)
+	polygon.set_texture(atlas)
+	polygon.set_texture_offset(Vector2(randf_range(0, a_size), randf_range(0, a_size)))
+	polygon.set_texture_repeat(CanvasItem.TEXTURE_REPEAT_ENABLED)
+	asteroid.set_mass((r0**2 + (r0 + bulk_r * size / v / 2)**2)**1.5)
+	asteroid.set_physics_material_override(material_rock)
+	asteroid.add_child(cpolygon)
+	asteroid.add_child(polygon)
+	return asteroid
--- main/main.gd
+++ main/main.gd
@@ -1,22 +1,20 @@
-extends Node
+extends Node2D
 
-var Asteroid = preload("res://asteroid/asteroid.gd")
-@onready var asteroids = Array()
+var AsteroidFactory = preload("res://asteroid/rock_asteroid_factory.gd")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
+	var factory = AsteroidFactory.new()
 	for i in range(50):
-		var asteroid = Asteroid.new(Asteroid.GenType.BULGE, [26, 250, 20, .3, 100, 6, .6])
+		var asteroid = factory.bulge_gen(26, 250, 20, .3, 100, 6, .6)
 		asteroid.set_angular_velocity(randf_range(-1, 1))
 		asteroid.set_position(Vector2(randf_range(-4000, 4000), randf_range(-4000, 4000)))
-		asteroids.append(asteroid)
 		add_child(asteroid)
 	for i in range(200):
-		var asteroid = Asteroid.new(Asteroid.GenType.SIMPLE, [14, 80, 5, .1])
+		var asteroid = factory.simple_gen(14, 80, 5, .1)
 		asteroid.set_angular_velocity(randf_range(-1, 1))
 		asteroid.set_position(Vector2(randf_range(-4000, 4000), randf_range(-4000, 4000)))
-		asteroids.append(asteroid)
 		add_child(asteroid)
 
 
